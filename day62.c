42621515eea48add4104052ae272e7fd6cbfec07
Saurav Raj Singh <mr.sauravsingh05@gmail.com>
2026-04-03 09:53:24 +0530

    62

---
+++ day62.c
@@ -0,0 +1,90 @@
+// Problem: Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+
+// Input:
+// - n (vertices)
+// - m (edges)
+// - edges (u, v)
+
+// Output:
+// - List of adjacency lists for each vertex
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph {
+    int V;
+    struct Node** adjList;
+};
+
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+
+    graph->adjList = (struct Node**)malloc(V * sizeof(struct Node*));
+
+    for (int i = 0; i < V; i++)
+        graph->adjList[i] = NULL;
+
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = graph->adjList[u];
+    graph->adjList[u] = newNode;
+
+
+    newNode = createNode(u);
+    newNode->next = graph->adjList[v];
+    graph->adjList[v] = newNode;
+}
+
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->V; i++) {
+        printf("%d -> ", i);
+        struct Node* temp = graph->adjList[i];
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int n, m;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    struct Graph* graph = createGraph(n);
+
+    int u, v;
+    printf("Enter edges (u v):\n");
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v);
+    }
+
+    printf("\nAdjacency List:\n");
+    printGraph(graph);
+
+    return 0;
+}
